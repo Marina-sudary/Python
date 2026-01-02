02374b9fce4f2fb0803686d90758ab6eedd7e9ce
Marina-sudary <mech694539@gmail.com>
2026-01-02 17:42:13 +0300

    Добавлена папка 06_lesson в ветку lesson6

---
+++ 06_lesson/click.py
@@ -0,0 +1,24 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+ 
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/ajax")
+
+    
+driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+driver.implicitly_wait(20)
+
+
+content = driver.find_element(By.CSS_SELECTOR, "#content")
+txt = content.find_element(By.CSS_SELECTOR, "p.bg-success").text
+print(txt)
+
+driver.quit()
