b54603db1911cf20ef4973e702a3a0fa2cffbd23
Marina-sudary <mech694539@gmail.com>
2026-01-20 22:07:56 +0300

    Сохранение текущих изменений перед переключением

--- lesson1/lesson_1_task_2.py
+++
@@ -1,5 +0,0 @@
-my_age = 56
-my_age = my_age + 3
-
-print("Мой возраст через 3 года", my_age)
-
--- lesson1/lesson_1_task_5.py
+++
@@ -1,5 +0,0 @@
-def prin_num(num):
-    print (num)     
-
-for i in range(11):
-    prin_num('88005553535')
